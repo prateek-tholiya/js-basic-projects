f7b5cf7ff6f55779c95ce7258e73f0ea5c5ea219
prateek-tholiya <148348873+prateek-tholiya@users.noreply.github.com>
2024-05-12 13:16:16 +0000

    js

---
+++ project4/script.js
@@ -0,0 +1,28 @@
+var elem = document.querySelectorAll("#elem")
+
+elem.forEach(function (val) {
+
+    val.addEventListener("mouseenter", function () {
+        val.childNodes[3].style.opacity = 1
+    });
+    val.addEventListener("mouseleave", function () {
+        val.childNodes[3].style.opacity = 0
+    });
+    val.addEventListener("mousemove", function (dets) {
+        val.childNodes[3].style.left = dets.x + "px"
+    })
+
+})
+// var elemimage = document.querySelector(".elem1 img")
+
+// elem1.addEventListener("mousemove",function(dets){
+//     elemimage.style.left = dets.x+"px"
+//     elemimage.style.top = dets.y+"px"
+
+// })
+// elem1.addEventListener("mouseenter",function(){
+//     elemimage.style.opacity = 1
+// })
+// elem1.addEventListener("mouseleave",function(){
+//     elemimage.style.opacity = 0
+// })
